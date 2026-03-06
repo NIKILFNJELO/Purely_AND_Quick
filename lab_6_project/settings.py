ae1ed354cbda11c770304a20f0a38a7ae949165b
NIKILFNJELO <_email@gmail.com>
2026-03-06 18:13:50 +0200

    Update project

--- lab_6_project/settings.py
+++ lab_6_project/settings.py
@@ -71,6 +71,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
